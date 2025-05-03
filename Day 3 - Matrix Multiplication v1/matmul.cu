6fbb7c09fcfcab12083c7f62b233cf01ec6d1034
adityaag24 <agrawaladitya192@gmail.com>
2025-05-03 22:50:13 +0530

    Added Day 6 Materials

--- Day 3 - Matrix Multiplication v1/matmul.cu
+++ Day 3 - Matrix Multiplication v1/matmul.cu
@@ -1,15 +1,19 @@
 #include <cuda.h>
 #include <stdio.h>
 #define TPB 32
-__global__ void matmul(int *a, int *b, int *c, int m, int k, int n){
+#define r_A 4096
+#define c_A 4096
+#define r_B 4096
+#define c_B 4096
+__global__ void matmul(int *a, int *b, int *c){
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     int sum = 0;
-    if (row < m && col < n) {
-        for (int i = 0; i < k; i++) {
-            sum += (a[row * k + i] * b[i * n + col]);
+    if (row < r_A && col < c_B) {
+        for (int i = 0; i < r_B; i++) {
+            sum += (a[row * r_B + i] * b[i * c_B + col]);
         }
-        c[row * n + col] = sum; 
+        c[row * c_B + col] = sum; 
     }
 }
 __host__ void printMatrix(int *a, int r, int c){
@@ -20,14 +24,14 @@ __host__ void printMatrix(int *a, int r, int c){
         printf("\n");
     }
 }
-__host__ void matmulHost(int *a, int *b, int *c, int m, int k, int n){
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
+__host__ void matmulHost(int *a, int *b, int *c){
+    for (int i = 0; i < r_A; i++){
+        for (int j = 0; j < c_B; j++){
             int sum = 0;
-            for (int l = 0; l < k; l++){
-                sum += (a[i * k + l]*b[l * n + j]);
+            for (int l = 0; l < c_A; l++){
+                sum += (a[i * c_A + l]*b[l * c_B + j]);
             }
-            c[i * n + j] = sum;
+            c[i * c_B + j] = sum;
         }
     }
 }
@@ -38,47 +42,42 @@ int main() {
     float time_ms = 0.0f;
     int *h_a, *h_b, *h_c;
     int *d_a, *d_b, *d_c;
-    int m, k, n;
-    scanf("%d %d %d ",&m, &k, &n);
-    h_a = (int *)malloc(m * k * sizeof(int));
-    h_b = (int *)malloc(k * n * sizeof(int));
-    h_c = (int *)malloc(m * n * sizeof(int));
-    cudaMalloc((void **)&d_a, m * k * sizeof(int));
-    cudaMalloc((void **)&d_b, k * n * sizeof(int));
-    cudaMalloc((void **)&d_c, m * n * sizeof(int));
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < k; j++) {
-            h_a[i * k + j] = rand() % 10;
+    h_a = (int *)malloc(r_A * c_A * sizeof(int));
+    h_b = (int *)malloc(r_B * c_B * sizeof(int));
+    h_c = (int *)malloc(r_A * c_B * sizeof(int));
+    cudaMalloc((void **)&d_a, r_A * c_A * sizeof(int));
+    cudaMalloc((void **)&d_b, r_B * c_B * sizeof(int));
+    cudaMalloc((void **)&d_c, r_A * c_B * sizeof(int));
+    for (int i = 0; i < r_A; i++){
+        for (int j = 0; j < c_A; j++) {
+            h_a[i * c_B + j] = rand() % 10;
         }
     }
-    for (int i = 0; i < k; i++) {
-        for (int j = 0; j < n; j++) {
-            h_b[i * n + j] = rand() % 10;
+    for (int i = 0; i < r_B; i++) {
+        for (int j = 0; j < c_B; j++) {
+            h_b[i * c_B + j] = rand() % 10;
         }
     }
-    cudaMemcpy(d_a, h_a, m * k * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, k * n * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_a, h_a, r_A * c_A * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, r_B * c_B * sizeof(int), cudaMemcpyHostToDevice);
     cudaEventRecord(start, 0);
-    dim3 grid((n + TPB - 1) / TPB, (m + TPB - 1) / TPB, 1);
+    dim3 grid((c_B + TPB - 1) / TPB, (r_A + TPB - 1) / TPB, 1);
     dim3 block(TPB, TPB, 1);
-    matmul<<<grid, block>>>(d_a, d_b, d_c, m, k, n);
+    matmul<<<grid, block>>>(d_a, d_b, d_c);
     cudaEventRecord(stop,  0);
     cudaEventSynchronize(stop);
-    cudaMemcpy(h_c, d_c, m * n * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_c, d_c, r_A * c_B * sizeof(int), cudaMemcpyDeviceToHost);
     cudaEventElapsedTime(&time_ms, start, stop);
-    printf("===Matrix A===\n");
-    printMatrix(h_a, m, k);
-    printf("===Matrix B===\n");
-    printMatrix(h_b, k ,n);
-    printf("===Matrix C===\n");
-    printMatrix(h_c, m, n);
+    // printMatrix(h_a, r_A, c_A);
+    // printMatrix(h_b, r_B, c_B);
+    // printMatrix(h_c, r_A, c_B);
     printf("Time Taken = %0.2f ms\n", time_ms);
-    int *v_c = (int *)malloc(m * n * sizeof(int));
-    matmulHost(h_a,h_b,v_c,m,k,n);
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
-            if (v_c[i * n + j] != h_c[i * n + j]){
-                printf("Output Differs for (%d,%d) = %d vs %d\n", i, j, v_c[i*n+j], h_c[i*n+j]);
+    int *v_c = (int *)malloc(r_A * c_B * sizeof(int));
+    matmulHost(h_a,h_b,v_c);
+    for (int i = 0; i < r_A; i++){
+        for (int j = 0; j < c_B; j++){
+            if (v_c[i * c_B + j] != h_c[i * c_B + j]){
+                printf("Output Differs for (%d,%d) = %d vs %d\n", i, j, v_c[i * c_B + j], h_c[i * c_B + j]);
                 return 0;
             }
         }
---
+++ Day 6 - Matrix Multiplication PMPP Exercises/matmul_per_thread_col.cu
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <cuda.h>
+#define TPB 32
+#define r_A 4096
+#define c_A 4096
+#define r_B 4096
+#define c_B 4096
+// Error checking macro
+#define CHECK_CUDA_ERROR(call) \
+{ \
+    cudaError_t err = call; \
+    if (err != cudaSuccess) { \
+        printf("CUDA Error: %s, line %d\n", cudaGetErrorString(err), __LINE__); \
+        exit(1); \
+    } \
+}
+__host__ void printMatrix(int *a, int r, int c){
+    for(int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++){
+            printf("%d ", a[i*c + j]);
+        }
+        printf("\n");
+    }
+}
+__host__ void matmulHost(int *a, int *b, int *c, int m, int k, int n){
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            int sum = 0;
+            for (int l = 0; l < k; l++){
+                sum += (a[i * k + l]*b[l * n + j]);
+            }
+            c[i * n + j] = sum;
+        }
+    }
+}
+__global__ void matmul(int *d_A, int *d_B, int *d_C) {
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    if (col < c_B) {
+        for (int i = 0; i < r_A; i++) {
+            int pVal = 0;
+            for (int l = 0; l < c_A; l++){
+                pVal += (d_A[i * c_A + l] * d_B[l * c_B + col]);
+            }
+            d_C[i * c_B + col] = pVal;
+        }
+    }
+}
+int main() {
+    int *h_A, *h_B, *h_C;
+    int *d_A, *d_B, *d_C;
+    cudaEvent_t start, stop;
+    float timeTaken;
+    h_A = (int *)malloc(r_A * c_A * sizeof(int));
+    h_B = (int *)malloc(r_B * c_B * sizeof(int));
+    h_C = (int *)malloc(r_A * c_B * sizeof(int));
+    CHECK_CUDA_ERROR(cudaEventCreate(&start));
+    CHECK_CUDA_ERROR(cudaEventCreate(&stop));
+    CHECK_CUDA_ERROR(cudaMalloc((void **)&d_A, r_A * c_A * sizeof(int)));
+    CHECK_CUDA_ERROR(cudaMalloc((void **)&d_B, r_B * c_B * sizeof(int)));
+    CHECK_CUDA_ERROR(cudaMalloc((void **)&d_C, r_A * c_B * sizeof(int)));
+    for (int i = 0; i < r_A; i++){
+        for (int j = 0; j < c_A; j++) {
+            h_A[i * c_A + j] = rand() % 10;
+        }
+    }
+    for (int i = 0; i < r_B; i++){
+        for (int j = 0; j < c_B; j++){
+            h_B[i * c_B + j] = rand() % 10;
+        }
+    }
+    dim3 g((c_B + TPB - 1)/TPB, 1, 1);
+    dim3 b(TPB, 1, 1);
+    cudaEventRecord(start, 0);
+    CHECK_CUDA_ERROR(cudaMemcpy(d_A, h_A, r_A * c_A * sizeof(int), cudaMemcpyHostToDevice));
+    CHECK_CUDA_ERROR(cudaMemcpy(d_B, h_B, r_B * c_B * sizeof(int), cudaMemcpyHostToDevice));
+    matmul<<<g, b>>>(d_A, d_B, d_C);
+    CHECK_CUDA_ERROR(cudaMemcpy(h_C, d_C, r_A * c_B * sizeof(int), cudaMemcpyDeviceToHost));
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    CHECK_CUDA_ERROR(cudaEventElapsedTime(&timeTaken, start, stop));
+    printf("Time Taken = %.2f ms\n", timeTaken);
+    // printMatrix(h_A, r_A, c_A);
+    // printMatrix(h_B, r_B, c_B);
+    // printMatrix(h_C, r_A, c_B);
+    int *v_C = (int *)malloc(r_A * c_B * sizeof(int));
+    matmulHost(h_A, h_B, v_C, r_A, c_A, c_B);
+#ifdef VERIFY
+    for (int i = 0; i < r_A; i++){
+        for (int j = 0; j < c_B; j++){
+            if (v_C[i * c_B + j] != h_C[i * c_B + j]){
+                printf("Output Differs for (%d,%d) = %d vs %d\n", i, j, v_C[i * c_B + j], h_C[i * c_B + j]);
+                return 0;
+            }
+        }
+    }
+    printf("Output Verified \n");
+#endif
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+    free(h_A);
+    free(h_B);
+    free(h_C);
+}
