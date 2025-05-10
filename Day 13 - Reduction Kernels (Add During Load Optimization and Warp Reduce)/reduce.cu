647dfd0c07ff4cd413017dc825da7e75125b08e0
adityaag24 <agrawaladitya192@gmail.com>
2025-05-10 23:22:20 +0530

    Added Day 13 Materials

---
+++ Day 13 - Reduction Kernels (Add During Load Optimization and Warp Reduce)/reduce.cu
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <cuda.h>
+#define THREADS_PER_BLOCK 128
+#define N 8192
+__device__ void warpReduce(volatile int *sdata, int tid) {
+    sdata[tid] += sdata[tid + 32];
+    sdata[tid] += sdata[tid + 16];
+    sdata[tid] += sdata[tid + 8];   
+    sdata[tid] += sdata[tid + 4];   
+    sdata[tid] += sdata[tid + 2];   
+    sdata[tid] += sdata[tid + 1];  
+}
+__global__ void reduce(int *g_iData, int *g_oData) {
+    extern __shared__ int sdata[];
+    // Load shared memory
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
+    sdata[tid] = g_iData[i] + g_iData[i + blockDim.x];
+    __syncthreads();
+ 
+    // Do reduction in shared memory
+    for (unsigned int s=blockDim.x/2; s>32; s>>=1) {
+        if (tid < s) {
+            sdata[tid] += sdata[tid + s];
+        }
+        __syncthreads();
+    }
+    if (tid < 32){
+        warpReduce(sdata, tid);
+    }
+    // Write result for this block to global memory
+    if (tid == 0) {
+        g_oData[blockIdx.x] = sdata[0];
+    }
+}
+void runReduction(int *hData, int h_result) {
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    int *g_iData = NULL;
+    int *g_oData = NULL;
+    int d_result = 0;
+    float timeTaken = 0.0f;
+    cudaMalloc(&g_iData, N * sizeof(int));
+    cudaMalloc(&g_oData, N * sizeof(int));
+    cudaMemcpy(g_iData, hData, N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaEventRecord(start, 0);
+    int blocks    = (N + THREADS_PER_BLOCK - 1)/ THREADS_PER_BLOCK / 2;
+    int oldBlocks = blocks;
+    while (blocks > 0) {
+        reduce<<<blocks, THREADS_PER_BLOCK, THREADS_PER_BLOCK * sizeof(int)>>>(g_iData,g_oData);
+        cudaMemcpy(g_iData, g_oData, N * sizeof(int), cudaMemcpyDeviceToDevice);
+        oldBlocks = blocks;
+        blocks    = blocks / THREADS_PER_BLOCK / 2;
+    }
+    if (blocks == 0 && oldBlocks != 1) {
+        reduce<<<1, oldBlocks/2, oldBlocks  * sizeof(int)>>>(g_iData, g_oData);
+    }
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&timeTaken, start, stop);
+    cudaMemcpy(&d_result, &g_oData[0], sizeof(int), cudaMemcpyDeviceToHost);
+    if (d_result == h_result) {
+        printf("Results Matched.\n");
+        printf("Time Taken: %.2f ms\n", timeTaken);
+        printf("Bandwidth: %.2f MB/s\n", (N * 2 * sizeof(int)) / (timeTaken * 1e6));
+    } else {
+        printf("Results Mismatched.\n");
+        printf("Expected: %d, Got: %d\n", h_result, d_result);
+    }
+    cudaFree(g_iData);
+    cudaFree(g_oData);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+}
+int main() {
+    int hData[N];
+    int hResult = 0;
+    for (int i = 0; i < N; i++){
+        hData[i] = rand() % 10;
+        hResult += hData[i];
+    }
+    runReduction(hData, hResult);
+}
--- README.md
+++ README.md
@@ -113,3 +113,12 @@ Code Repository for the 100 Days of GPU Challenge
 > Reading: -
 
 >> Reading about reduction kernels from [this link](https://drive.google.com/file/d/18MxiGsXZ6QPj7q1r1RFkn2DoFHc7jNb6/view)
+
+## Day 13
+> Coding: -
+
+>> Coded the Reduction Kernel that uses sequenced addressing to reduce bank conflicts and increases per thread activity by adding an element while loading. Also added a warp reduce optimization to optimize the execution for the last warp.
+
+> Reading: -
+
+>> Reading about reduction kernels from [this link](https://drive.google.com/file/d/18MxiGsXZ6QPj7q1r1RFkn2DoFHc7jNb6/view)
